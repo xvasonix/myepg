9c5ed11c553a578411b49ad0a25b6f2c9866b169
include <include@daum.net>
2023-08-08 22:05:08 +0900

    edit: epg_update.sh

--- file/epg_update.sh
+++ file/epg_update.sh
@@ -1,3 +1,3 @@
 export LANG=en_US.utf8
 PLUGIN_HOME=$1
-epg2xml run --config %PLUGIN_HOME%/file/epg2xml.json --channelfile %PLUGIN_HOME%/file/Channel.json --xmlfile %PLUGIN_HOME%/file/xmltv.xml
+epg2xml run --config $PLUGIN_HOME/file/epg2xml.json --channelfile $PLUGIN_HOME/file/Channel.json --xmlfile $PLUGIN_HOME/file/xmltv.xml
